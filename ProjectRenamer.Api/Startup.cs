33dba4b5ea311cf20ba4c0c5494b8a5adffb8f71
Adem Catamak <adem.catamak@trendyol.com>
2018-07-13 09:08:46 +0300

    alternatices package update

--- ProjectRenamer.Api/Startup.cs
+++ ProjectRenamer.Api/Startup.cs
@@ -13,9 +13,11 @@ namespace ProjectRenamer.Api
 {
     public class Startup
     {
-        private static readonly string _solutionName = "ProjectRenamer.Api";
+        private const string SOLUTION_NAME = "ProjectRenamer.Api";
 
         private const string ALLOWED_ORIGINS_SECTION_NAME = "AllowedOrigins";
+        private const string SWAGGER_VERSION = "v-1";
+
 
         private IConfiguration Configuration { get; }
         private IHostingEnvironment HostingEnvironment { get; }
@@ -31,9 +33,7 @@ namespace ProjectRenamer.Api
         {
             services.AddCors();
             services.AddMvc(options => { options.Filters.Add<GeneralExceptionFilter>(); }).AddJsonOptions(options => { options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver(); });
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info {Title = _solutionName, Version = "v-1"}); });
-
-            services.AddTransient<ICryptoEngine, CryptographyEngine>();
+            services.AddSwaggerGen(c => { c.SwaggerDoc(SWAGGER_VERSION, new Info { Title = SOLUTION_NAME, Version = SWAGGER_VERSION }); });
 
             services.Configure<AllowedOriginsConstants>(Configuration.GetSection(ALLOWED_ORIGINS_SECTION_NAME));
 
@@ -55,7 +55,7 @@ namespace ProjectRenamer.Api
             app.UseAuthentication();
             app.UseMvc();
             app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{_solutionName} V1"); });
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint($"/swagger/{SWAGGER_VERSION}/swagger.json", $"{SOLUTION_NAME} {SWAGGER_VERSION}"); });
         }
     }
 }
